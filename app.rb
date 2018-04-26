bd8c599a2920f53030c4c4e7bc5f59bb7fb9d049
omarahmed103 <omariahmed103@gmail.com>
2018-04-26 15:27:30 -0400

    Email is working now

--- app.rb
+++ app.rb
@@ -11,14 +11,13 @@ erb :index
 end
 
 get '/cookies' do
-    "Hello World"
 
     erb :cookies
 end
 
-get '/cupcakes' do
+get '/cupcake' do
 
-    erb :cucakes
+    erb :cupcake
 end
 
 get '/muffins' do
@@ -30,3 +29,26 @@ get '/catlog' do
 
     erb :catlog
 end
+
+
+
+
+
+
+post "/catlog" do 
+    description = " <h2> Cookies $5.99/ Dozen  <br>  Muffins $6.99/ Dozen <br> Cakes $59.99 / Pound </h2>"
+    puts params.inspect
+    from = Email.new(email:'oahmed103140@gmail.com')
+    to = Email.new(email:params[:email])
+    subject = params[:name]
+    content = Content.new(type: 'text/html', value: description )
+    mail = Mail.new(from, subject, to, content )
+    
+    sg = SendGrid::API.new(api_key:ENV['SENDGRID_API_KEY'])
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    puts response.status_code
+    puts response.body
+    # puts response.parsed_body
+    puts response.headers
+    erb :index
+    end
